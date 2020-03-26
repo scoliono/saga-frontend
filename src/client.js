e68c8452f78127c060f930560d67f56b78050469
James Shiffer <basedvioletfag@waifu.club>
2020-03-26 09:05:52 -0700

    Proper socket.io hostname

--- src/client.js
+++ src/client.js
@@ -19,7 +19,8 @@ import Echo from 'laravel-echo';
 window.io = require('socket.io-client');
 window.Echo = new Echo({
 	broadcaster: 'socket.io',
-	host: window.location.hostname + ':6000'
+	host: window.location.hostname === 'localhost' ? 'localhost:6001'
+		: 'io.' + window.location.hostname.replace(/^www\./, '')
 });
 
 sapper.start({
